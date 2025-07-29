fc6999c1daa7f99335a00dd4f5156f5112b64984
Oliviaheckert <107218530+Oliviaheckert@users.noreply.github.com>
2025-07-29 16:36:27 -0400

    Update avatar-toggle.js
    Updated with avatar toggles

--- avatar-toggle.js
+++ avatar-toggle.js
@@ -1,1 +1,13 @@
-abc
+const image = document.getElementById('avatar');
+
+image.addEventListener('click', function () {
+    if (image.src.match('./assets/santa.png')) {
+        image.src = './assests/reindeer.png';
+    } else if (image.src.match('./assests/reindeer.png')) {
+        image.src './assests/bear.png';
+    } else if (image.src.match('./assests/bear.png')) {
+        image.src = './assests/cookie.png';
+    } else if {
+        image.src = './assests/santa.png';
+    }
+});
